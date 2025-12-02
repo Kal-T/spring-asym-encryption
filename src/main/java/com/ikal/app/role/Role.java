38c354db22552b6ca720ee6b7deb3a0af9d358a4
thihahtetzaw <htetzaw619t@prestigein.com>
2025-12-02 19:44:56 +0900

    Initialize project

---
+++ src/main/java/com/ikal/app/role/Role.java
@@ -0,0 +1,25 @@
+package com.ikal.app.role;
+
+import com.ikal.app.common.BaseEntity;
+import com.ikal.app.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "ROLES")
+public class Role extends BaseEntity {
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
